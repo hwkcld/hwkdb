f7936c85112ef9d2b680e22e0c8c2339edfe6bf5
hwkcld <hoth.peter+hwk@gmail.com>
2025-09-17 16:32:00 +0800

    Update setup-hwkdb.sh

--- setup-hwkdb.sh
+++ setup-hwkdb.sh
@@ -7,6 +7,12 @@ containername=hwkdb
 set -o pipefail
 
 if [[ "$1" == "host" ]]; then
+
+    if [[ "$2" == "" ]]; then
+       echo "Please specify the machine type e.g. cpu1-2gb"
+       exit 1
+    fi
+
     echo "Adding user: ${pguser} ..."
     sudo useradd -ms /bin/bash ${pguser}
     if [ $? -eq 0 ]; then
@@ -19,11 +25,18 @@ if [[ "$1" == "host" ]]; then
 
     sudo runuser -l ${pguser} -c "wget -O ~/setup-hwkdb.sh https://raw.githubusercontent.com/hwkcld/hwkdb/main/setup-hwkdb.sh && chmod 700 ~/setup-hwkdb.sh"
 
-    sudo runuser -l ${pguser} -c "~/setup-hwkdb.sh"
-	
-	echo $?
-	
+    sudo runuser -l ${pguser} -c "~/setup-hwkdb.sh $2"
+
+    echo $?
+
 else
+
+    if [[ "$1" == "" ]]; then
+       echo "Please specify the machine type e.g. cpu1-2gb"
+       exit 1
+    fi
+
+    machine=$1
     export XDG_RUNTIME_DIR=/run/user/${UID}
     echo "XDG_RUNTIME_DIR = ${XDG_RUNTIME_DIR}"
 
@@ -31,29 +44,28 @@ else
     podman volume create ${dbdata}
 
     echo "Download the default postgres.conf"
-    wget -O ~/postgres.conf https://raw.githubusercontent.com/hwkcld/hwkdb/main/postgres.conf
+    wget -O ~/postgres.conf https://raw.githubusercontent.com/hwkcld/hwkdb/main/${machine}/postgres.conf
 
     echo "Create directory for quadlet"
     mkdir -p ~/.config/containers/systemd
 
     echo "Download the default quadlet file"
-    wget -O ~/.config/containers/systemd/hwkdb.container https://raw.githubusercontent.com/hwkcld/hwkdb/main/hwkdb.container
-    
+    wget -O ~/.config/containers/systemd/hwkdb.container https://raw.githubusercontent.com/hwkcld/hwkdb/main/${machine}/hwkdb.container
+
     echo Create the hwkdb service
-    systemctl --user daemon-reload  
+    systemctl --user daemon-reload
 
     echo "Start the service using systemd i.e. auto reload even after system restart"
     systemctl --user start hwkdb.service
 
     if [ $? -eq 0 ]; then
-	    echo "waiting for database server ..."
-	    sleep 10
+            echo "waiting for database server ..."
+            sleep 10
         podman exec -it ${containername} psql -U ${pguser} -c "\password ${pguser};"
         if [ $? -ne 0 ]; then
-		    echo "You can manually set the password again"
-		fi
+                    echo "You can manually set the password again"
+                fi
     else
-        echo "failed."  
+        echo "failed."
     fi
-        
 fi
